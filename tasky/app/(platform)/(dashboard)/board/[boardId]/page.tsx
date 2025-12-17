4bbd804bd642c64186c921ac85c7d646a3037a68
Oksana Arakelyan <oksana31204@mail.ru>
2025-12-17 22:27:51 +0300

    Страница доски ч.1
    Создание отдельной страницы доски, навигационная панель.

---
+++ tasky/app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,12 @@
+const BoardIdPage = () => {
+    return (
+        <div>
+            Board ID!
+            Board ID!
+            Board ID!
+            Board ID!
+        </div>
+    );
+};
+
+export  default BoardIdPage;
--- tasky/app/(platform)/(dashboard)/organization/[organisationId]/layout.tsx
+++ tasky/app/(platform)/(dashboard)/organization/[organisationId]/layout.tsx
@@ -1,16 +1,23 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server"; 
+
 import { OrgControl } from "./_components/org-control";
 
-const OrganizationIdLayout = ({
-    children
-}: {
-    children: React.ReactNode;
-}) => {
-    return (
-        <>
-            <OrgControl/>
-            { children }
-        </>
-    );
+export async function generateMetadata() {
+  const { orgSlug } = await auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
 };
 
 export default OrganizationIdLayout;
